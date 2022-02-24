9134b90d54b19a919da7e11dd08bc3c87d15bcd4
rafayhabibullah <rafay_habibullah@hotmail.com>
2022-02-25 01:55:38 +0500

    add initial code structure

---
+++ src/main/java/com/zooplus/zooplusassignment/ZooplusAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.zooplus.zooplusassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooplusAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooplusAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zooplus/zooplusassignment/ZooplusAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zooplus.zooplusassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooplusAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
